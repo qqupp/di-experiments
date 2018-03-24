07a8f8ae8e5ba6c4709a9b1e259260155e79e36b
Paolo Picci <paolo.picci@sky.uk>
2018-03-24 10:45:18 +0000

    Empty Sbt module
    wip
    
    Simple Constructor Injection implementation.
    
    Renaming pkg
    
    New Example
    
    correcting formatting
    
    wip
    
    wip
    
    Constructor injection Example

---
+++ src/main/scala/qqupp/dependency_injection/Main.scala
@@ -0,0 +1,18 @@
+package qqupp.dependency_injection
+
+object MaybeSoftWord2020 extends App {
+  import Config._
+
+  wordProcessor.composeNewDocument
+}
+
+object Config {
+  val NonsenseDictionary = Map(
+    "Foo" -> "Just a foo?",
+    "Bar" -> "Bar bar, BAR!",
+    "Baz" -> "Bazinga."
+  )
+  val wordDictionary = DefaultWordDictionary("English", NonsenseDictionary)
+  val punctuationRules = DefaultPunctuationRules("English", wordDictionary)
+  val wordProcessor = WordProcessor(wordDictionary, punctuationRules)
+}
---
+++ src/main/scala/qqupp/dependency_injection/package.scala
@@ -0,0 +1,8 @@
+package qqupp
+
+package object dependency_injection {
+  type Definition = String
+  type Word = String
+  type Lang = String
+  type Syllable = String
+}
