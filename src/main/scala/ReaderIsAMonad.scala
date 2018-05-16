2ce753bd6b19c971e54dc033680c180075bb91a9
Paolo <paolo.picci@gmail.com>
2018-05-16 20:49:15 +0100

    tidy up

--- src/main/scala/ReaderIsAMonad.scala
+++ src/main/scala/ReaderIsAMonad.scala
@@ -4,60 +4,75 @@
 object ReaderIsAMonad {
   import Utils._
 
-  case class Reader[E, T](run: E => T)
-  def unit[T]: T =>  Reader[String, T] = (x: T) => Reader(_ => x )
-  def flatMap[E, T1, T2]: (Reader[E,T1]) => (T1 => Reader[E, T2]) => Reader[E, T2] =
-    (r) => (f) => Reader(e => ( f ( r.run(e) ) ).run(e))
+  case class Reader_[E, T](run: E => T)
+  def unit[T]: T =>  Reader_[String, T] = (x: T) => Reader_(_ => x )
+  def flatMap[E, T1, T2]: (Reader_[E,T1]) => (T1 => Reader_[E, T2]) => Reader_[E, T2] =
+    (r) => (f) => Reader_(e => ( f ( r.run(e) ) ).run(e))
 
-  def compose[E, A, B, C](f: A => Reader[E, B], g: B => Reader[E, C]): A => Reader[E, C] =
+
+  def compose[E, A, B, C](f: A => Reader_[E, B], g: B => Reader_[E, C]): A => Reader_[E, C] =
     (x: A) => flatMap(f(x))(g)
 
   object Proof {
-    // now lets help the type inference making fix some types...
+    // fix some types
     type E = Int
-    type R[X] = Reader[E, X]
+    type R[X] = Reader[X]
     type A = Int
     type B = Int
     type C = Int
     type D = Int
-    val c = 10
-    type F = A => R[A]
-    type G = A => R[A]
+    type F = A => R[B]
+    type G = B => R[C]
 
-    // and chose a particular instance for the polimorphic functions
+    case class Reader[X](run: E => X)
+
     val unit: A => R[A] =
-      (x: A) => Reader[E, A](_ => x)
+      (x: A) => Reader[A](_ => x)
 
     val flatMap:(R[A]) => (A => R[B]) => R[B] =
-      ((r: R[A]) => (f: F) => Reader[E, B]((e: E )=> ( f ( r.run(e) ) ).run(e)))
+      ((r: R[A]) => (f: F) => Reader[B]((e: E )=> ( f ( r.run(e) ) ).run(e)))
 
     val compose: (F, G) => ( A => R[A]) =
       (f: F, g: G) => (x: A) => flatMap(f(x))(g)
 
-    // a monadic function
-    val h: G = (a: A) => Reader[E, C]((e: E) => c)
 
+    def c(a: A, e: E): C = ???
+
+    val h: G = (a: A) => Reader[C]((e_1: E) => c(a,e_1))
+
+    /*
+      In order to have a proof that reader is a monad we need to satisfy the monad laws.
+
+      - left identity
+          compose(unit, h)  =  h
+
+      - right identity
+          compose(unit, h)  =  h
+
+      - associative property
+          compose(compose(f1, g1), k1)  =  compose(f1, compose(g1, k1))
+     */
 
     /*
       Left Identity
      */
-    compose(unit, h)                                          =?= h
-    ((f: F, g: G) => (x: A) => flatMap (f(x))(g)) (unit, h)   =?= h
-    ((x: A) => flatMap (unit(x)) (h))                                                   =?= h
+    compose(unit, h) =?= h
+    ((f: F, g: G) => (x: A) => flatMap (f(x))(g)) (unit, h)                                                      =?= h
+    ((x: A) => flatMap (unit(x)) (h))                                                                            =?= h
     // subst unit
-    ((x: A) => flatMap (((x1: A) => Reader[E,A](_ => x1))(x)) (h))                      =?= h
-    ((x: A) => flatMap (Reader[E,A](_ => x)) (h))                                       =?= h
+    ((x: A) => flatMap (((x1: A) => Reader[A](_ => x1))(x)) (h))                                                 =?= h
+    ((x: A) => flatMap (Reader[A](_ => x)) (h))                                                                  =?= h
     // subst flatMap def
-    ((x: A) => ((r: R[A]) => (f: F) => Reader[E, A]((e: E ) => ( f ( r.run(e) ) ).run(e))) (Reader[E,A](_ => x)) (h))             =?= h
-    ((x: A) => ((f: F) => Reader[E, A]((e: E ) => ( f ( (Reader[E,A](_ => x)).run(e) ) ).run(e))) (h))                            =?= h
-    ((x: A) => Reader[E, A]((e: E ) => ( (h) ( (Reader[E,A](_ => x)).run(e) ) ).run(e)) )                                         =?= h
-    ((x: A) => Reader[E, A]((e: E ) => ( (h) (x) ).run(e)))                                                                       =?= h
+    ((x: A) => ((r: R[A]) => (f: F) => Reader(( e: E ) => ( f ( r.run(e) ) ).run(e))) (Reader[A](_ => x)) (h))   =?= h
+    ((x: A) => ((f: F) => Reader((e: E ) => ( f ( (Reader[A](_ => x)).run(e) ) ).run(e))) (h))                   =?= h
+    ((x: A) => Reader((e: E ) => ( (h) ( (Reader[A](_ => x)).run(e) ) ).run(e)) )                                =?= h
+    ((x: A) => Reader((e: E ) => ( (h) (x) ).run(e)))                                                            =?= h
     // subst h definition
-    ((x: A) => Reader[E, A]((e: E ) => ( ((A: A) => Reader((e: E) => c)) (x) ).run(e)))                                           =?= h
-    ((x: A) => Reader[E, A]((e: E ) => ( Reader((e: E) => c) ).run(e)))                                                           =?= h
-    ((x: A) => Reader[E, A]((e: E ) => c))                                                                                        =?= h
+    ((x: A) => Reader((e: E ) => ( ((a: A) => Reader[C]((e_1: E) => c(a,e_1))) (x) ).run(e)))                    =?= h
+    ((x: A) => Reader((e: E ) => Reader((e_1: E) => c(x,e_1)).run(e)))                                           =?= h
+    ((x: A) => Reader((e: E ) => c(x, e)))                                                                       =?= h
 
-    compose(unit, h) === ((x: A) => Reader[E, A]((e: E ) => c)) === h
+    compose(unit, h) === ((x: A) => Reader[C]((e: E ) => c(x, e))) === h
     QED
 
 
@@ -65,23 +80,23 @@ object ReaderIsAMonad {
       Right identity
      */
     compose(h, unit) =?= h
-    ((f: G, g: F) => (x: A) => flatMap(f(x))(g)) (h, unit)     =?= h
-    ((x: A) => flatMap((h)(x))(unit))                          =?= h
+    ((f: G, g: F) => (x: A) => flatMap(f(x))(g)) (h, unit)                                                        =?= h
+    ((x: A) => flatMap((h)(x))(unit))                                                                             =?= h
 
     // subst flatMap
-    ((x: A) => ((r: R[A]) => (f: F) => Reader[E, A]((e: E )=> ( f ( r.run(e) ) ).run(e))) (h(x)) (unit))                        =?= h
-    ((x: A) => ((f: F) => Reader[E, A]((e: E )=> ( f ( (h(x)).run(e) ) ).run(e)))  (unit))                                      =?= h
+    ((x: A) => ((r: R[A]) => (f: F) => Reader((e: E )=> ( f ( r.run(e) ) ).run(e))) (h(x)) (unit))                =?= h
+    ((x: A) => ((f: F) => Reader((e: E )=> ( f ( (h(x)).run(e) ) ).run(e)))  (unit))                              =?= h
     // subst h
-    ((x: A) => ((f: F) => Reader[E, A]((e: E )=> ( f ( ( Reader[E, A]((e: E) => c)).run(e) ) ).run(e)))  (unit))                =?= h
-    ((x: A) => ((f: F) => Reader[E, A]((e: E )=> ( f ( c ) ).run(e)))  (unit))                                                  =?= h
+    ((x: A) => ((f: F) => Reader((e: E )=> ( f ( ( Reader[C]((e_1: E) => c(x, e_1))).run(e) ) ).run(e)))  (unit)) =?= h
+    ((x: A) => ((f: F) => Reader((e: E )=> ( f ( c(x, e) ) ).run(e)))  (unit))                                    =?= h
     // subst f
-    ((x: A) => (Reader[E, A]((e: E )=> ( unit ( c ) ).run(e))))                                                                 =?= h
+    ((x: A) => (Reader((e: E )=> ( unit ( c(x, e) ) ).run(e))))                                                   =?= h
     // subst unit
-    ((x: A) => (Reader[E, A]((e: E )=> ( ((x: A) => Reader[E, A](_ => x)) ( c ) ).run(e))))                                     =?= h
-    ((x: A) => (Reader[E, A]((e: E )=> (Reader[E, A](_ => c)).run(e))))                                                         =?= h
-    ((x: A) => Reader[E, A]((e: E )=> c))                                                                                       =?= h
+    ((x: A) => (Reader((e: E )=> ( ((x: A) => Reader(_ => x)) ( c(x, e) ) ).run(e))))                             =?= h
+    ((x: A) => (Reader((e: E )=> (Reader(_ => c(x, e))).run(e))))                                                 =?= h
+    ((x: A) => Reader((e: E )=> c(x, e)))                                                                         =?= h
 
-    compose(h, unit) === ((x: A) => Reader[E, A]((e: E )=> c)) === h
+    compose(h, unit) === ((x: A) => Reader((e: E )=> c(x, e))) === h
     QED
 
     def f1: A => R[A] = (a1: A) => Reader((e: E) => a1 + 1)
@@ -89,6 +104,7 @@ object ReaderIsAMonad {
     def k1: A => R[A] = (a3: A) => Reader((e: E) => a3 ^ 3)
 
     type FF = A => R[A]
+
     /*
       Associative
      */
@@ -99,18 +115,18 @@ object ReaderIsAMonad {
     ((x1: A) => flatMap((((f: FF, g: FF) => (x: A) => flatMap(f(x))(g))(f1, g1))(x1))(k1))                    =?=  compose(f1, compose(g1, k1))
     ((x1: A) => flatMap( ((x: A) => flatMap(f1(x))(g1))(x1) )(k1))                                            =?=  compose(f1, compose(g1, k1))
     ((x1: A) => flatMap( flatMap(f1(x1))(g1) )(k1))                                                           =?=  compose(f1, compose(g1, k1))
-    ((x1: A) => ((r: R[A]) => (f: F) => Reader[E, A]((e: E )=> ( f ( r.run(e) ) ).run(e))) ( flatMap(f1(x1))(g1) ) (k1))                                               =?=  compose(f1, compose(g1, k1))
-    ((x1: A) => ((f: F) => Reader[E, A]((e: E )=> ( f ( ( flatMap(f1(x1))(g1) ).run(e) ) ).run(e)))  (k1))                                                             =?=  compose(f1, compose(g1, k1))
-    ((x1: A) => Reader[E, A]((e: E )=> ( (k1) ( ( flatMap(f1(x1))(g1) ).run(e) ) ).run(e))  )                                                                          =?=  compose(f1, compose(g1, k1))
-    ((x1: A) => Reader[E, A]((e1: E )=> ( (k1) ( ( flatMap(f1(x1))(g1)).run(e1) ) ).run(e1))  )                                                                        =?=  compose(f1, compose(g1, k1))
-    ((x1: A) => Reader[E, A]((e1: E )=> ( (k1) ( (  ((r: R[A]) => (f: F) => Reader[E, A]((e: E )=> ( f ( r.run(e) ) ).run(e))) (f1(x1)) (g1)).run(e1) ) ).run(e1))  )  =?=  compose(f1, compose(g1, k1))
-    ((x1: A) => Reader[E, A]((e1: E )=> ( (k1) ( (  ( (f: F) => Reader[E, A]((e: E )=> ( f ( (f1(x1)).run(e) ) ).run(e)))  (g1)).run(e1) ) ).run(e1))  )               =?=  compose(f1, compose(g1, k1))
-    ((x1: A) => Reader[E, A]((e1: E )=> ( (k1) ( Reader[E, A]( (e: E ) => ( (g1) ( (f1(x1)).run(e) ) ).run(e))  .run(e1) ) ).run(e1))  )                               =?=  compose(f1, compose(g1, k1))
-    ((x1: A) => Reader[E, A]((e1: E )=> ( (k1) ( ( (e: E ) => ( (g1) ( (f1(x1)).run(e) ) ).run(e)) (e1) ) ).run(e1))  )                                                =?=  compose(f1, compose(g1, k1))
-    ((x1: A) => Reader[E, A]((e1: E )=> ( (k1) ( ( ( (g1) ( (f1(x1)).run(e1) ) ).run(e1))  ) ).run(e1))  )                                                             =?=  compose(f1, compose(g1, k1))
+    ((x1: A) => ((r: R[A]) => (f: F) => Reader((e: E )=> ( f ( r.run(e) ) ).run(e))) ( flatMap(f1(x1))(g1) ) (k1))                                       =?=  compose(f1, compose(g1, k1))
+    ((x1: A) => ((f: F) => Reader((e: E )=> ( f ( ( flatMap(f1(x1))(g1) ).run(e) ) ).run(e)))  (k1))                                                     =?=  compose(f1, compose(g1, k1))
+    ((x1: A) => Reader((e: E )=> ( (k1) ( ( flatMap(f1(x1))(g1) ).run(e) ) ).run(e))  )                                                                  =?=  compose(f1, compose(g1, k1))
+    ((x1: A) => Reader((e1: E )=> ( (k1) ( ( flatMap(f1(x1))(g1)).run(e1) ) ).run(e1))  )                                                                =?=  compose(f1, compose(g1, k1))
+    ((x1: A) => Reader((e1: E )=> ( (k1) ( (  ((r: R[A]) => (f: F) => Reader((e: E )=> ( f ( r.run(e) ) ).run(e))) (f1(x1)) (g1)).run(e1) ) ).run(e1)))  =?=  compose(f1, compose(g1, k1))
+    ((x1: A) => Reader((e1: E )=> ( (k1) ( (  ( (f: F) => Reader((e: E )=> ( f ( (f1(x1)).run(e) ) ).run(e)))  (g1)).run(e1) ) ).run(e1))  )             =?=  compose(f1, compose(g1, k1))
+    ((x1: A) => Reader((e1: E )=> ( (k1) ( Reader( (e: E ) => ( (g1) ( (f1(x1)).run(e) ) ).run(e))  .run(e1) ) ).run(e1))  )                             =?=  compose(f1, compose(g1, k1))
+    ((x1: A) => Reader((e1: E )=> ( (k1) ( ( (e: E ) => ( (g1) ( (f1(x1)).run(e) ) ).run(e)) (e1) ) ).run(e1))  )                                        =?=  compose(f1, compose(g1, k1))
+    ((x1: A) => Reader((e1: E )=> ( (k1) ( ( ( (g1) ( (f1(x1)).run(e1) ) ).run(e1))  ) ).run(e1))  )                                                     =?=  compose(f1, compose(g1, k1))
 
     val lhsProof: A => R[A] =
-      (x1: A) => Reader[E, A]{ (e1: E ) =>
+      (x1: A) => Reader{ (e1: E ) =>
         k1(
           g1(
             f1(
@@ -120,36 +136,38 @@ object ReaderIsAMonad {
         ).run(e1)
       }
 
+
     compose(compose(f1, g1), k1) === lhsProof
 
-    compose(f1, compose(g1, k1))      =?=  lhsProof
+
+    compose(f1, compose(g1, k1))                                                                         =?=  lhsProof
     ((f: FF, g: FF) => (x: A) => flatMap(f(x))(g)) (f1, compose(g1, k1))                                 =?=  lhsProof
     ( (x1: A) => ((flatMap (f1(x1))) (compose(g1, k1))) )                                                =?=  lhsProof
     ( (x1: A) => flatMap (f1(x1)) (((f: F, g: G) => (x: A) => flatMap(f(x))(g))(g1, k1)) )               =?=  lhsProof
     ( (x1: A) => flatMap (f1(x1)) ((x: A) => flatMap (g1(x)) (k1)) )                                     =?=  lhsProof
-    ( (x1: A) => flatMap (f1(x1)) ((x: A) => ((r2: R[A]) => (f2: F) => Reader[E, A]((e2: E )=> ( f2 ( r2.run(e2) ) ).run(e2))) (g1(x)) (k1)) )          =?=  lhsProof
-    ( (x1: A) => flatMap (f1(x1)) ((x3: A) => Reader[E, A]((e2: E )=> ( (k1) ( (g1(x3)).run(e2) ) ).run(e2))  ) )                                       =?=  lhsProof
-    ( (x1: A) => ((r: R[A]) => (f: F) => Reader[E, A]((e: E )=> ( f ( r.run(e) ) ).run(e))) (f1(x1)) ((x3: A) => Reader[E, A]((e2: E )=> ( (k1) ( (g1(x3)).run(e2) ) ).run(e2))  ) )          =?=  lhsProof
+    ( (x1: A) => flatMap (f1(x1)) ((x: A) => ((r2: R[A]) => (f2: F) => Reader((e2: E )=> ( f2 ( r2.run(e2) ) ).run(e2))) (g1(x)) (k1)) )                                 =?=  lhsProof
+    ( (x1: A) => flatMap (f1(x1)) ((x3: A) => Reader((e2: E )=> ( (k1) ( (g1(x3)).run(e2) ) ).run(e2))  ) )                                                              =?=  lhsProof
+    ( (x1: A) => ((r: R[A]) => (f: F) => Reader((e: E )=> ( f ( r.run(e) ) ).run(e))) (f1(x1)) ((x3: A) => Reader((e2: E )=> ( (k1) ( (g1(x3)).run(e2) ) ).run(e2)) ))   =?=  lhsProof
     
     ((x1: A) => 
-      ((r: R[A]) => (f: F) => Reader[E, A]((e: E )=> ( f ( r.run(e) ) ).run(e)))  (f1(x1))  ((x3: A) => Reader[E, A]((e2: E )=> ( (k1) ( (g1(x3)).run(e2) ) ).run(e2))  )
+      ((r: R[A]) => (f: F) => Reader((e: E )=> ( f ( r.run(e) ) ).run(e)))  (f1(x1))  ((x3: A) => Reader((e2: E )=> ( (k1) ( (g1(x3)).run(e2) ) ).run(e2))  )
       )          =?=  lhsProof
     
     ((x1: A) =>
-      Reader[E, A]((e: E )=> ( ((x3: A) => Reader[E, A]((e2: E )=> ( (k1) ( (g1(x3)).run(e2) ) ).run(e2))  ) ( (f1(x1)).run(e) ) ).run(e))    
+      Reader((e: E )=> ( ((x3: A) => Reader((e2: E )=> ( (k1) ( (g1(x3)).run(e2) ) ).run(e2))  ) ( (f1(x1)).run(e) ) ).run(e))
       )          =?=  lhsProof
 
     ((x1: A) =>
-      Reader[E, A](
+      Reader(
         (e: E ) =>
-          (  (Reader[E, A]((e2: E )=> ( (k1) ( (g1(( (f1(x1)).run(e) ))).run(e2) ) ).run(e2))  )  ).run(e)
+          (  (Reader((e2: E )=> ( (k1) ( (g1(( (f1(x1)).run(e) ))).run(e2) ) ).run(e2))  )  ).run(e)
       )
       )          =?=  lhsProof
 
     ((x1: A) =>
-      Reader[E, A](
+      Reader(
         (e: E ) =>
-          Reader[E, A](
+          Reader(
             (e2: E )=> ( (k1) ( (g1(( (f1(x1)).run(e) ))).run(e2) ) ).run(e2)).run(e)
       )
       )          =?=  lhsProof
@@ -157,9 +175,9 @@ object ReaderIsAMonad {
 
 
     ((x1: A) =>
-      Reader[E, A](
+      Reader(
         (e: E ) =>
-          Reader[E, A](
+          Reader(
             (e2: E )=>
               k1( (g1(( (f1(x1)).run(e) ))).run(e2) ).run(e2))
             .run(e)
@@ -168,7 +186,7 @@ object ReaderIsAMonad {
 
 
     ((x1: A) =>
-      Reader[E, A](
+      Reader(
         (e: E ) =>
               k1( (g1(( (f1(x1)).run(e) ))).run(e) ).run(e))
 
@@ -179,7 +197,7 @@ object ReaderIsAMonad {
 
     {
       (x1: A) =>
-        Reader[E, A]{(e: E ) =>
+        Reader{(e: E ) =>
           k1(
             g1(
               f1(
@@ -192,7 +210,7 @@ object ReaderIsAMonad {
     }  ===  lhsProof === compose(compose(f1, g1), k1) ===  compose(f1, compose(g1, k1)) === {
 
       (x1: A) =>
-        Reader[E, A] { (e1: E) =>
+        Reader{ (e1: E) =>
           k1(
             g1(
               f1(
